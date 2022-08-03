e64fc1c7b649866eb3f05b37910f40811b874be6
x7md <x7mdNet@gmail.com>
2022-08-03 19:33:12 +0300

    add head element

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,14 +1,26 @@
 /** @jsx h */
 import { h } from "preact";
 import { tw } from "@twind";
-import Desert  from "../islands/Desert.tsx"
-export default function Home() {
+import { HeadElement } from "../components/HeadElement.tsx";
+import Desert  from "../islands/Desert.tsx";
+
+
+export default function Home(ctx: any) {
+  const { data, url } = ctx;
   return (
     <div class={tw`min-h-screen bg-[#EEA559]`}>
+      <HeadElement
+        description="Saudi Deno Merch"
+        // image={url.href + "og-image.png"}
+        title="Saudi Deno"
+        url={url}
+      />
       <div class={tw`flex flex-col `}>
       <Desert />
       <div class={tw`z-10 pt-10 text-center`}>
-        <h1 class={tw`text-3xl text-[#ffffff] font-bold`}>Deno on the desert!</h1>
+        <h1 class={tw`text-3xl text-[#ffffff] font-bold`}>
+          Deno on the desert!
+        </h1>
       </div>
       {/* <img
         src="/logo.svg"
